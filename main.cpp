09eafe0ab24b79b4128af8cd869301d0762635fe
root <root@localhost.localdomain>
2018-10-17 00:35:34 +0900

    Restart

--- main.cpp
+++ main.cpp
@@ -1,38 +1,17 @@
 #include "mbed.h"
-#include "EthernetInterface.h"
- 
-#define MBED_DEV_IP       "192.168.0.52"
-#define MBED_DEV_MASK     "255.255.255.0"
-#define MBED_DEV_GW       "0.0.0.0"
-#define ECHO_SERVER_PORT   5000
 
- 
-int main (void) {
-    EthernetInterface eth;
-    eth.init(MBED_DEV_IP, MBED_DEV_MASK, MBED_DEV_GW); //Assign a device ip, mask and gateway
-    eth.connect();
-    printf("IP Address is %s\n", eth.getIPAddress());
-    
-    TCPSocketServer server;
-    server.bind(ECHO_SERVER_PORT);
-    server.listen();
-    
+DigitalOut led1(LED1);
+
+int main() {
+    printf("Hello World?\n\r");
+    char ch[150] = "";
     while (true) {
-        printf("\nWait for new connection...\n");
-        TCPSocketConnection client;
-        server.accept(client);
-        client.set_blocking(false, 1500); // Timeout after (1.5)s
+        scanf("%s",&ch);
+        printf(">%s\n\r",ch);
         
-        printf("Connection from: %s\n", client.get_address());
-        char buffer[256];
-        while (true) {
-            int n = client.receive(buffer, sizeof(buffer));
-            if (n <= 0) break;
-            
-            client.send_all(buffer, n);
-            if (n <= 0) break;
-        }
-        
-        client.close();
+        led1 = !led1;
+        wait(0.5);
     }
 }
+
+
